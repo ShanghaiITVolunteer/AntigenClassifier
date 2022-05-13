48476dbf7f243525e7aca7d9e3b04e1e04827e8d
wj-Mcat <1435130236@qq.com>
2022-05-13 11:20:11 +0800

    update serving module

--- serving.py
+++ serving.py
@@ -14,6 +14,16 @@ os.makedirs(cache_dir, exist_ok=True)
 searcher, id_map = Searcher()
 
 
+def map_result(result):
+    new_result = {
+        "positive": [],
+        "negative": []
+    }
+    for item in result.get('positive', []):
+        new_result['positive'].append(float(item[-1]))
+    for item in result.get('negative', []):
+        new_result['negative'].append(float(item[-1]))
+    return new_result 
 
 
 @app.route('/antigen-images', methods=['POST'])
@@ -24,6 +34,7 @@ def hello():
     #result = classifier.predict(file)
     img = cv2.imread(file)
     result = antigener_classification_update_1(img, searcher, id_map)
+    result = map_result(result)
     return jsonify(dict(code=200, data=result))
 
 app.run(host='0.0.0.0', port=8089)
